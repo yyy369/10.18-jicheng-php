e7c507d9ac4222a2c27449c21ae262482fb7f1c4
yyy369 <42286804+yyy369@users.noreply.github.com>
2018-10-19 07:54:13 +0800

    zuoye

---
+++ Good.class.php
@@ -0,0 +1,27 @@
+<?php
+header(string)
+Class Goods{
+     var $name;
+     var $num;
+     var $pay;
+    function __construct($name,$num,$pay){
+    	$this->name=$name;
+    	$this->num=$num;
+    	$this->pay=$pay;
+    }
+    public function printall(){
+    	echo "商品名:".$this->name.'数量:'.$this->num.'价格:'.$this->pay."<br/>";
+    }
+} 
+class phone extends Goods{
+    public $brand;
+    function __construct($name,$num,$pay,$brand){
+    parent::__construct();
+    $this->brand=$brand;
+    }
+    public function printall(){
+    echo "商品名:".$this->name.'数量:'.$this->num.'价格:'.$this->pay.'品牌:'.$this->brand."<br/>";
+    }
+}
+$p1=new phone('手机',10,2000,'华为');
+$p1->printall();
